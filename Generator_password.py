1b2510be158bd659c616f32cd9f1c0ea801623be
Saara Joldoshova <99747271+joldoshovas@users.noreply.github.com>
2022-02-17 21:16:03 +0600

    Update Generator_password.py

--- Generator_password.py
+++ Generator_password.py
@@ -5,12 +5,13 @@ adjectives = ['sleepy', 'slow', 'hot', 'cold', 'big', 'red', 'orange', 'yellow',
 nouns = ['apple', 'dinosaur', 'ball', 'cat', 'goat', 'dragon', 'car', 'duck', 'panda']
 print('Добро пожаловать!')
 while True:
-    adjective = random.choice(adjectives)
-    noun = random.choice(nouns)
-    number = random.randrange(0, 100)
-    special_char = random.choice(string.punctuation)
-    password = adjective + noun + str(number) + special_char
-    print('Новый пароль: %s' % password )
+    for num in range(3):
+        adjective = random.choice(adjectives)
+        noun = random.choice(nouns)
+        number = random.randrange(0, 100)
+        special_char = random.choice(string.punctuation)
+        password = adjective + noun + str(number) + special_char
+        print('Новый пароль: %s' % password )
     response = input('Сгенерировать новый пароль? Введите д или н:')
     if response == 'н':
         break
