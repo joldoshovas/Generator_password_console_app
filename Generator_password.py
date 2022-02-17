7a28469eb55cbf1eec3c007e28e7d9c60511a8fa
Saara Joldoshova <99747271+joldoshovas@users.noreply.github.com>
2022-02-17 21:04:25 +0600

    Add files via upload

---
+++ Generator_password.py
@@ -0,0 +1,16 @@
+import random
+import string
+from urllib import response
+adjectives = ['sleepy', 'slow', 'hot', 'cold', 'big', 'red', 'orange', 'yellow', 'green', 'blue', 'old', 'white', 'free', 'brave']
+nouns = ['apple', 'dinosaur', 'ball', 'cat', 'goat', 'dragon', 'car', 'duck', 'panda']
+print('Добро пожаловать!')
+while True:
+    adjective = random.choice(adjectives)
+    noun = random.choice(nouns)
+    number = random.randrange(0, 100)
+    special_char = random.choice(string.punctuation)
+    password = adjective + noun + str(number) + special_char
+    print('Новый пароль: %s' % password )
+    response = input('Сгенерировать новый пароль? Введите д или н:')
+    if response == 'н':
+        break
